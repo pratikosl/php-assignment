736abf7fafcdda1181888e98e88c7a0c31d06a5b
test <pratik.sharma@opensenselabs.com>
2023-08-11 11:57:28 +0530

    modiied

--- Assignment-2/insert.php
+++ Assignment-2/insert.php
@@ -1,19 +1,15 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-  $name=$_POST['name'];
-  $email=$_POST['email'];
-  $cookie_value=$name;
-  setcookie("username", $cookie_value, time() + 15, "", "localhost");
-  if(isset($_COOKIE['username'])){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $cookie_value = $name;
+  setcookie("username", $cookie_value, time() + 120, "", "localhost");
+  $_COOKIE['username'] = $name;
+  if (isset($_COOKIE['username'])) {
     echo "Thanks for subscribing, " . strtoupper($_COOKIE['username']) . ".";
-    setcookie("username", "", time() - 5, "", "localhost");
-    header("refresh: 10;URL=login.php");
-  }
-
-  else{
+    setcookie("username", "", time() - 120, "", "localhost");
+    header("refresh: 120;URL=login.php");
+  } else {
     echo "Incorrect Details Filled";
   }
 }
-
-
-?>
